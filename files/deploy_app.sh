f81c3177ccf84788f9844c35e393b066a70389d7
riteshkumar87 <riteshkumar87@gmail.com>
2024-09-11 15:22:05 +0000

    first commit

---
+++ files/deploy_app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+
+# Script to deploy a very simple web application.
+# The web app has a customizable image and some text.
+
+cat << EOM > /var/www/html/index.html
+<html>
+  <head><title>TerraWorld_RV!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+
+  <!-- BEGIN -->
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><h2>Hello Hello!</h2></center>
+  Welcome to ${PREFIX}'s app. 
+  <center><h2>This is cool....</h3></center>
+  <!-- END -->
+
+  </div>
+  </body>
+</html>
+EOM
+
+echo "Script complete."
